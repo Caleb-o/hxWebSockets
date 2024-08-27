147614730d53979578cff1f51e5e4d2ff6c02fe8
Caleb-o <caleb.ottohayes@gmail.com>
2024-08-27 13:59:26 +1000

    Update abstract enums

--- hx/ws/Types.hx
+++ hx/ws/Types.hx
@@ -5,18 +5,16 @@ import haxe.io.Bytes;
 #if js
 typedef BinaryType = js.html.BinaryType;
 #else
+enum abstract BinaryType(String) {
+	var ARRAYBUFFER = "arraybuffer";
 
-@:enum abstract BinaryType(String) {
-    var ARRAYBUFFER = "arraybuffer";
-
-    @:to public function toString() {
-        return this;
-    }
+	@:to public function toString() {
+		return this;
+	}
 }
-
 #end
 
 enum MessageType {
-    BytesMessage(content: Buffer);
-    StrMessage(content: String);
+	BytesMessage(content:Buffer);
+	StrMessage(content:String);
 }
